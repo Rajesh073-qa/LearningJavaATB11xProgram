ff2f045571658c23183cf241bb7d868dc334cb76
Rajesh073-qa <144097713+Rajesh073-qa@users.noreply.github.com>
2025-03-11 12:32:19 +0530

    using printf print the multiplication of 9

---
+++ src/ex02_JavaBasics/Lab026_printf_assignment.java
@@ -0,0 +1,9 @@
+package ex02_JavaBasics;
+
+public class Lab026_printf_assignment {
+    public static void main(String[] args) {
+        int a = 9;
+        System.out.printf("%d %d %d %d %d %d %d %d %d %d", a, a*2, a*3, a*4, a*5, a*6, a*7, a*8, a*9,a*10);
+
+    }
+}
