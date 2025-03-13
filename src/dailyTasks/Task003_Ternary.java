b6d8c33c95fb7ff3e29d4c5c79e0c8d597a8c823
Rajesh073-qa <144097713+Rajesh073-qa@users.noreply.github.com>
2025-03-13 15:30:53 +0530

    Task003 - To find a grade

---
+++ src/dailyTasks/Task003_Ternary.java
@@ -0,0 +1,17 @@
+package dailyTasks;
+/*Write a program that calculates and displays the letter grade for a given numerical score
+(e.g., A, B, C, D, or F) based on the following grading scale:
+
+A: 90-100
+B: 80-89
+C: 70-79
+D: 60-69
+F: 0-59 */
+public class Task003_Ternary {
+    public static void main(String[] args) {
+      int score =97;
+        String grade = (score>=90)? "A" :(score>=80)? "B" : (score>=70)? "C" : (score>=60)? "D" : "F" ;
+        System.out.println(grade);
+
+    }
+}
