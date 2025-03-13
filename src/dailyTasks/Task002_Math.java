12c51847a0564bc9c8249870729279fba610f365
Rajesh073-qa <144097713+Rajesh073-qa@users.noreply.github.com>
2025-03-13 15:16:22 +0530

    Literals,operator, Type casting, Ternary operator

---
+++ src/dailyTasks/Task002_Math.java
@@ -0,0 +1,9 @@
+package dailyTasks;
+
+public class Task002_Math {
+    public static void main(String[] args) {
+  int  X = 10, Y = 10, Z = 10 ;
+ double result= Math.cbrt((Math.pow(X,2)+(Math.pow(Y,2)))-(Math.abs(Z)));
+        System.out.println(result);
+}
+}
