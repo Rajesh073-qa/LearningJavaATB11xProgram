7ceff6fdf3fae8a9b6b1e67261c960b5263b99ef
Rajesh073-qa <144097713+Rajesh073-qa@users.noreply.github.com>
2025-03-15 08:59:22 +0530

    Increment & Decrement

---
+++ src/dailyTasks/Task004_In_De_operator.java
@@ -0,0 +1,9 @@
+package dailyTasks;
+
+public class Task004_In_De_operator {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(++a + a++ + a++);
+        System.out.println(a);
+    }
+}
